9501e39cb256e7cd9ffd91f650aba1e2b9b8a954
lere dakshak mamman <dshak0672@gmail.com>
2025-04-08 02:30:07 +0000

    add movies functionality implement

--- movielist.js
+++ movielist.js
@@ -4,3 +4,20 @@
 // Delete movie
 // Sort by year or title
 
+let movies = [];
+
+function addMovie(title, genre, year) {
+  let movieid = movies.length + 1;
+  let newMovie = {
+    id: movieid,
+    title: title,
+    genre: genre,
+    year: year,
+    watched: false
+  };
+  movies.push(newMovie); 
+  console.log("Movie added:", newMovie);
+}
+addMovie("The Avengers", "Action", 2012);
+addMovie("The Lion King", "Animation", 1994);
+addMovie("Mr Bones", "comedy", 2010);
