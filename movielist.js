d22b3fc927c7d3a572884aa3497a96302dc63093
lere dakshak mamman <dshak0672@gmail.com>
2025-04-08 03:19:37 +0000

    mark movie as watch function engage

--- movielist.js
+++ movielist.js
@@ -21,3 +21,17 @@ function addMovie(title, genre, year) {
 addMovie("The Avengers", "Action", 2012);
 addMovie("The Lion King", "Animation", 1994);
 addMovie("Mr Bones", "comedy", 2010);
+
+// mark movie as watched/unwatched
+
+function markAsWatched(movieid) {
+  let movie = movies.find(muvie => muvie.id === movieid);
+  if (movie) {
+    movie.watched = !movie.watched;
+    console.log("Movie watch:", movie);
+  } else {
+    console.log("Movie not found");
+  }
+}
+markAsWatched(1);
+markAsWatched(2);
